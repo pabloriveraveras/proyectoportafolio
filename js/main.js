fccd6fd88edf572afb2a09c10afa1320f80c99f6
pabloriveraveras <pablo.rivera.pf@gmail.com>
2023-07-30 19:00:02 -0300

    modificacion en enlace de js y font size

--- js/main.js
+++ js/main.js
@@ -1,24 +1,27 @@
-/* container 3 */
+/* prueba de js */
 
-var carouselImages = document.querySelectorAll(".tarjeta img");
-
-carouselImages.forEach(function(image) {
-  image.addEventListener("click", function() {
-    var projectUrl = this.getAttribute("data-url");
-    window.location.href = projectUrl;
-  });
-});
+alert("Bienvenidx a mi portafolio")
 
 
-/* efecto boton container 2 */
 
-const button = document.getElementById('#btncv');
-  button.addEventListener('mouseover', () => {
-    button.style.transform = 'scale(1.2)';
-  });
-  button.addEventListener('mousedown', () => {
-    button.style.backgroundColor = 'red';
-  });
+/* accion al hacer click sobre fotografia de perfil */
+
+function cambiarImagen() {
+  var imagenElement = document.getElementById('fotoprincipal');
+  var nuevaImagen = 'img/fotopersonal.jpg';
+  var nuevaAlt = 'Otra Imagen';
+
+  // Verificar si la imagen actual es la imagen original
+  if (imagenElement.src.endsWith('perfil.jpeg')) {
+      nuevaImagen = 'img/fotopersonal.jpg';
+      nuevaAlt = 'Otra Imagen';
+  } else {
+      nuevaImagen = 'img/perfil.jpeg';
+      nuevaAlt = 'fotopersonal';
+  }
+
+  imagenElement.src = nuevaImagen;
+  imagenElement.alt = nuevaAlt;
+}
 
 
- 
